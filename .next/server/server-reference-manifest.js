292bc9fbe92c8affd69a7055d1730185abafeab0
majd ismail <majdismail@majds-MacBook-Air.local>
2026-02-18 01:26:01 +0400

    Fix landing QR stage centering + spacing

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"YFVAyFkGelFaamtOv6Uo9nBfzFEZ8lSfPFelX9I9ghU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"JoDgvvMhK3hCWAudTsPQ+13dVAHuHzBK5NCaJgY8MuI=\"}"
